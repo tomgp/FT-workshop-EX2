b0ae87650b75872721b72dab35b0729437764c10
tomgp <tom.g.pearson@gmail.com>
2015-10-16 11:02:41 +0100

    setup example 2

--- source/main.js
+++ source/main.js
@@ -1,4 +1,10 @@
 'use strict';
 require('babelify/polyfill'); //remove this line if you don't care about ES6 pollyfils
+var d3 = require('d3');
 
-console.log('Hey! Ho! Let\'s go!'); //remove this line if you don't care about The Ramones
+d3.csv('data.csv', main);
+
+function main(data){
+	var dateFormat = d3.time.format('%Y-%m-%d');
+	console.log(data);
+}
