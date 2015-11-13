523f1fe9a2bea0a9ff02dedf41aeafa1191c3601
Tom Pearson <tom.g.pearson@gmail.com>
2015-11-13 10:53:12 +0000

    update to match docs

--- source/main.js
+++ source/main.js
@@ -1,22 +1,12 @@
 'use strict';
 require('babelify/polyfill'); //remove this line if you don't care about ES6 pollyfils
 var d3 = require('d3');
+var processElement = require('./process-data.js');
 
 d3.csv('data.csv', main);
 
 function main(data){
-	var processed = data.map( function(d){
-		var dateFormat = d3.time.format('%Y-%m-%d');
-		var total = Number( d.yes ) + Number( d.no );
-		return {
-			date: dateFormat.parse( d.date ),
-			yes: Number( d.yes ),
-			no: Number( d.no ),
-			total: total,
-			yesPct: 100/ total * Number( d.yes ),
-			noPct: 100/ total * Number( d.no )
-		};
-	});
+	var processed = data.map(processElement);
 	var filtered = processed.filter( function(d){
 		return (d.date.getFullYear() == 2001);
 	} );
